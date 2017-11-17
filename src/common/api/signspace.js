46b60378823ca737f8c3f7d03ae49f4d261ee8fb
jamestthompson3 <jamestthompson3@gmail.com>
2017-11-18 00:12:28 +0200

    [+] Adds common components folder, working on requests

--- src/common/api/signspace.js
+++ src/common/api/signspace.js
@@ -1,4 +1,4 @@
-import request from 'superagent'
+import { request } from 'superagent'
 import Cookies from 'js-cookie'
 
 
@@ -6,10 +6,12 @@ const BASEURL = 'https://signspace.beta.tilaajavastuu.fi/api/v1/users/me/'
 
 
 export const makeRequest = endpoint => {
-  // Cookies.set('session_id','e4618d365ec342bb8a3f5a67acf5d3df', { domain: 'app.signspace.com' })
-  Cookies.set('name', 'e4618d365ec342bb8a3f5a67acf5d3df')
+  Cookies.set('session_id','e4618d365ec342bb8a3f5a67acf5d3df')
+  // Cookies.set('session_id', 'e4618d365ec342bb8a3f5a67acf5d3df')
+
   return request
     .get(`${BASEURL}${endpoint}`)
-    .set('Access-Control-Allow-Origin', '*')
+    .withCredentials()
+    .set('Access-Control-Allow-Origin', 'http://localhost:3000')
 }
 
