a470feee32d51a89fb7c70493a83e73398a73924
Kirill Kubryakov <kubryakov@gmail.com>
2017-11-18 22:24:05 +0200

    [*] Switches to HashRouter for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
