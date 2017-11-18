9b47bf3d5d517783819fdf8ad884052b78435f89
jamestthompson3 <jamestthompson3@gmail.com>
2017-11-18 13:24:18 +0200

    Testing deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-import ContactsPage from './components/ContractsPage'
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path='/' component={ContactsPage} />
-        </Switch>
-      </Router>
+      <div>
+        <h1>HELLO THERE</h1>
+      </div>
     )
   }
 }
