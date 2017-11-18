50fffa867dac6ce112ce5f09e1d89766cedef4c9
Carlos Andres Mariscal Melgar <carlos.mariscal.melgar@gmail.com>
2017-11-18 16:03:56 +0200

    [+] date handler for json response [+] single entity fetching

--- flask/config.py
+++ flask/config.py
@@ -4,6 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
 # email server
 MAIL_SERVER = 'your.mailserver.com'
 MAIL_PORT = 25
--- flask/main.py
+++ flask/main.py
@@ -1,21 +1,92 @@
 
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Column, Table, ForeignKey
+from sqlalchemy import Integer, String, DateTime
+import sqlalchemy
+import os
+import re
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
-from wrkr_mail import send_email
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask import render_template
+from flask_mail import Message
+from decorators import async
+from config import ADMINS
+import json
+import datetime
+date_handler = lambda obj: ( obj.isoformat()
+                             if isinstance(obj, (datetime.datetime, datetime.date))
+                             else None)
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
-db_connect = create_engine('sqlite:///wrkr.db')
+def get_postgres(ip, port):
+    # engine = sqlalchemy.create_engine(f'postgresql+psycopg2://postgres:postgres@{ip}:{port}')
+    engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:postgres@{}:{}'.format(ip,port ))
+    return engine
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    send_async_email(app, msg)
+
+
+def follower_notification(followed, follower):
+    send_email("[Potential customer] %s is now following you!" % follower.nickname,
+               ADMINS[0],
+               [followed.email],
+               render_template("follower_email.txt",
+                               user=followed, follower=follower),
+               render_template("follower_email.html",
+                               user=followed, follower=follower))
+
+def create_db(connection_string):
+    if connection_string.startswith('sqlite'):
+        db_file = re.sub("sqlite.*:///", "", connection_string)
+        # os.makedirs(os.path.dirname(db_file), exist_ok=True)
+    engine = sqlalchemy.create_engine(connection_string)
+    return engine
+
+conn_string = 'sqlite:///wrkr.db'
+
+engine = get_postgres("127.0.0.1", 5432)
+
 app = Flask(__name__)
 api = Api(app)
+mail = Mail(app)
+db = SQLAlchemy(app)
 
+# @app.route('/employees')
 class Employees(Resource):
     def get(self):
-        conn = db_connect.connect() # connect to database
+        # conn = db_connect.connect() # connect to database
+        conn = get_postgres("127.0.0.1", 5432)
         query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        return {'employees': [json.dumps(i[0], default= date_handler )for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+    def put(self, name):
+        conn = db_connect.connect()
+        query = conn.execute("Insert into employees (name) {}".format(name))
+        return None
+
+class Materials(Resource):
+    def get(self):
+        # conn = db_connect.connect() # connect to database
+        conn = get_postgres("127.0.0.1", 5432)
+        query = conn.execute("select * from material") # This line performs query and returns json result
+        return {'materials': [json.dumps(i, default= date_handler )for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+    def put(self, name):
+        conn = db_connect.connect()
+        query = conn.execute("Insert into employees (name) {}".format(name))
+        return None
 
 class Employees_Name(Resource):
     def get(self, employee_id):
@@ -26,8 +97,10 @@ class Employees_Name(Resource):
         
 
 api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
 api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+api.add_resource(Materials, '/materials') # Route_1
+# api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
 
 if __name__ == '__main__':
