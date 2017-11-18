ca7b664bfd2704efab04c16cf511deab218950a7
jamestthompson3 <jamestthompson3@gmail.com>
2017-11-19 00:31:11 +0200

    [+] Adds custom UI to checkboxes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,20 +5,60 @@ import { connect } from 'react-redux'
 import * as actions from './actions'
 
 const TodoWrapper = styled.div`
-  border: 1px solid;
   width: 100%;
   margin: auto;
   display: flex;
   flex-direction: column;
+  align-items: center;
   li {
     flex:1;
     display: block;
   }
 `
-const TodoBox = styled.input.attrs({
-  type: 'checkbox'
-})`
-  border: 1px solid #c1c1c1;
+const TodoBox = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  user-select: none;
+  p {
+    padding-top: 2px;
+  }
+  input {
+    position: absolute;
+    opacity: 0;
+  }
+  input:checked ~ span {
+    background-color: ${p => p.theme.primaryColor};
+  }
+  input:checked ~ span:after {
+    display: block;
+}
+span:after {
+    left: 6px;
+    top: 0px;
+    width: 5px;
+    height: 12px;
+    border: solid #322F4A;
+    border-width: 0 3px 3px 0;
+    transform: rotate(45deg);
+}
+`
+const Checkmark = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 18px;
+  width: 20px;
+  background-color: transparent;
+  border: 2px solid;
+  border-radius: 5px;
+  &:after {
+    display: none;
+    content: "";
+    position: absolute;
+  }
 `
 class Todo extends Component {
 
@@ -29,8 +69,11 @@ class Todo extends Component {
         <ul>
           {contract.items.map(item =>
             <li key={item.id}>
-              <TodoBox />
-              <label>{item.text}</label>
+              <TodoBox>
+                <p>{item.text}</p>
+                <input type="checkbox" />
+                <Checkmark />
+              </TodoBox>
             </li>
           )}
         </ul>
